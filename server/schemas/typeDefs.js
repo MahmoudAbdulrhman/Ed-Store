aab58833874a3baa6d0bf3145c1cff91d1e4672e
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2021-05-27 23:15:07 -0700

    create typeDefs for the database

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,50 @@ const {gql} = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Query {
-    helloWorld: String
+type Category {
+    id: ID,
+    name: String
 }
+
+type Product {
+    id: ID,
+    name: String,
+    description: String,
+    image: String,
+    quantity: int,
+    price: Float,
+    category: Category
+}
+
+type User {
+    id: ID,
+    firstname: String,
+    lastname: String,
+    email: String,
+    orders: [orders] 
+}
+
+type Auth {
+    token: ID,
+    user: User
+}
+
+type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
+  }
 `;
 
 // export the typeDefs 
